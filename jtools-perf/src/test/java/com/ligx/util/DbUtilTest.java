20214de9c8a1b738b4edcb6ac6d65451cadc11df
ligongxing <ligongxing001@lianjia.com>
2018-11-26 18:39:04 +0800

    Modified: unit test

--- jtools-perf/src/test/java/com/ligx/util/DbUtilTest.java
+++ jtools-perf/src/test/java/com/ligx/util/DbUtilTest.java
@@ -10,6 +10,6 @@ public class DbUtilTest {
 
     @Test
     public void write() {
-        DbUtil.write("jtools_demo,ClassName=ProfilingTestService,Method=ProfilingTestService.profilingTestV2 QPS=1i,Avg=577i,Min=186i,Max=917i,Count=9i,TP50=752i,TP90=917i,TP95=917i,TP99=917i,TP999=917i,TP9999=917i,TP99999=917i,TP100=917i 1538118444679000000");
+        DbUtil.write("quality_worker_input,ClassName=ImageHashCodeService,Method=ImageHashCodeService.getImageHashCodeWithRetry QPS=1i,Avg=577i,Min=186i,Max=917i,Count=9i,TP50=752i,TP90=917i,TP95=917i,TP99=917i,TP999=917i,TP9999=917i,TP99999=917i,TP100=917i 1543228380285000000");
     }
 }
