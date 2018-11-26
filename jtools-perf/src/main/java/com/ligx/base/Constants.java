acf88a4c453c3c50e4eb2150e857490f406ac8d9
ligongxing <ligongxing001@lianjia.com>
2018-11-26 18:29:26 +0800

    Modified

--- jtools-perf/src/main/java/com/ligx/base/Constants.java
+++ jtools-perf/src/main/java/com/ligx/base/Constants.java
@@ -9,7 +9,7 @@ public class Constants {
     public static final int MIN_BACKUP_RECORDERS_COUNT = 1;
     public static final int MAX_BACKUP_RECORDERS_COUNT = 8;
 
-    public static final int MAX_METHOD_TAG_ID = 1024 * 128;
+    public static final int MAX_METHOD_TAG_ID = 1000;
 
     public static final String PROPERTIES_FILE_NAME = "monitor.properties";
 
