7e393c9ee986fc06e10c1cd9b7bb06f7852d43ac
ligongxing <604353886@qq.com>
2021-05-28 20:59:09 +0800

    modify

--- jtools-demo/src/test/java/com/ligx/string/字符串中字符的所有组合.java
+++ jtools-demo/src/test/java/com/ligx/string/字符串中字符的所有组合.java
@@ -6,31 +6,31 @@ package com.ligx.string;
  */
 public class 字符串中字符的所有组合 {
 
-    public static void combination(char[] string) {
-        if (string == null)
+    private static void combination(char[] chars) {
+        if (chars == null || chars.length == 0) {
             return;
-        StringBuilder sb = new StringBuilder();     //用StringBuilder容器来保存组合字符，因为它是动态变化的
-        for (int i = 1; i <= string.length; ++i) {  //由于组合可以是1个字符的组合，2个字符的字符…一直到n个字符的组合
-            combination(string, 0, i, sb);
+        }
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i <= chars.length; i++) {
+            combination(chars, 0, i, sb);
         }
     }
 
-    //下面用递归实现
-    public static void combination(char[] string, int begin, int number, StringBuilder sb) {
-        if (number == 0) {     //递归结束的条件
+    private static void combination(char[] chars, int begin, int num, StringBuilder sb) {
+        if (num == 0) {
             System.out.println(sb);
             return;
         }
-        if (begin == string.length)
+        if (begin == chars.length) {
             return;
+        }
 
-        sb.append(string[begin]);
-        combination(string, begin + 1, number - 1, sb);
+        sb.append(chars[begin]);
+        combination(chars, begin + 1, num - 1, sb);
 
         sb.deleteCharAt(sb.length() - 1);
-        combination(string, begin + 1, number, sb);
+        combination(chars, begin + 1, num, sb);
     }
-
 
     public static void main(String[] args) {
         String str = "abcd";
