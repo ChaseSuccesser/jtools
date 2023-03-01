bfaf54bede75d97ee9bbc6d5a4a84b51067b1d04
ligongxing <ligongxing@meituan.com>
2023-03-01 22:01:27 +0800

    modify

--- jtools-demo/src/test/java/com/ligx/list/找出倒数第4个元素.java
+++ jtools-demo/src/test/java/com/ligx/list/找出倒数第4个元素.java
@@ -1,12 +1,15 @@
 package com.ligx.list;
 
+import org.junit.Assert;
+
 /**
  * @author: ligongxing.
  * @date: 2021年04月12日.
  */
 public class 找出倒数第4个元素 {
 
-    public Link getLast4thOne(Link head, int k) {
+    // 1 2 3 4 5 6
+    private static Link getLast4thOne(Link head, int k) {
         if (head == null || k == 0) {
             return null;
         }
@@ -24,5 +27,21 @@ public class 找出倒数第4个元素 {
         }
 
         return second;
+    }
+
+
+    public static void main(String[] args) {
+        Link link7 = new Link(null, 7);
+        Link link6 = new Link(link7, 6);
+        Link link1 = new Link(link6, 5);
+        Link link2 = new Link(link1, 4);
+        Link link3 = new Link(link2, 3);
+        Link link4 = new Link(link3, 2);
+        Link link5 = new Link(link4, 1);
+
+
+        Link link = getLast4thOne(link5, 4);
+        Assert.assertTrue(link.data == 4);
+        System.out.println(link.data);
     }
 }
