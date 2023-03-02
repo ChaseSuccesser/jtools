d0b51580832dd38e458bccf26d9f2def612073d7
ligongxing <ligongxing@meituan.com>
2023-03-02 16:52:58 +0800

    modify

--- jtools-demo/src/test/java/com/ligx/tree/Node.java
+++ jtools-demo/src/test/java/com/ligx/tree/Node.java
@@ -18,6 +18,10 @@ public class Node {
     public Node() {
     }
 
+    public Node(int data) {
+        this.data = data;
+    }
+
     public Node(int data, Node left, Node right) {
         this.data = data;
         this.left = left;
