bb0c7d4e71bf0809ab404a57ac30ab9eabe0e41f
ligongxing <ligongxing@meituan.com>
2023-03-04 15:42:53 +0800

    modify

--- jtools-demo/src/test/java/com/ligx/array/找到两个有序数组的中位数.java
+++ jtools-demo/src/test/java/com/ligx/array/找到两个有序数组的中位数.java
@@ -6,43 +6,42 @@ package com.ligx.array;
  */
 public class 找到两个有序数组的中位数 {
 
-    private static int findKth(int[] a, int[] b) {
-        int m = a.length;
-        int n = b.length;
-        int total = m + n;
-        if (total % 2 != 0) {
-            return findKth(a, m, b, n, total / 2 + 1, 0, 0);
-        } else {
-            return (findKth(a, m, b, n, total / 2, 0, 0) + findKth(a, m, b, n, total / 2 + 1, 0, 0)) / 2;
-        }
+    private static double findMiddle(int[] arr1, int[] arr2) {
+        int m = arr1.length;
+        int n = arr2.length;
+        int left = (m + n + 1) / 2;
+        int right = (m + n + 2) / 2;
+        return (findKth(arr1, 0, m - 1, arr2, 0, n - 1, left) + findKth(arr1, 0, m - 1, arr2, 0, n - 1, right)) * 0.5;
     }
 
-    private static int findKth(int[] a, int m, int[] b, int n, int k, int headA, int headB) {
-        if (m > n) {
-            findKth(b, n, a, m, k, headB, headA);
+    private static int findKth(int[] arr1, int start1, int end1, int[] arr2, int start2, int end2, int k) {
+        int len1 = end1 - start1 + 1;
+        int len2 = end2 - start2 + 1;
+
+        if (len2 < len1) {
+            return findKth(arr2, start2, end2, arr1, start1, end1, k);
         }
-        if (m == 0) {
-            return b[k - 1 + headB];
+
+        if (len1 == 0) {
+            return arr2[start2 + k - 1];
         }
+
         if (k == 1) {
-            return Math.min(a[headA], b[headB]);
+            return Math.min(arr1[start1], arr2[start2]);
         }
-        int pa = Math.min(k / 2, m);
-        int pb = k - pa;
-        int newPa = headA + (pa - 1);
-        int newPb = headB + (pb - 1);
-        if (a[newPa] < b[newPb]) {
-            return findKth(a, m - pa, b, n, k - pa, headA += pa, headB);
-        } else if (a[newPa] > b[newPb]) {
-            return findKth(a, m, b, n - pb, k - pb, headA, headB += pb);
+
+        int i = start1 + Math.min(len1, k / 2) - 1;
+        int j = start2 + Math.min(len2, k / 2) - 1;
+        if (arr1[i] > arr2[j]) {
+            return findKth(arr1, start1, end1, arr2, j + 1, end2, k - (j - start2 + 1));
         } else {
-            return a[newPa];
+            return findKth(arr1, i + 1, end1, arr2, start2, end2, k - (i - start1 + 1));
         }
     }
 
     public static void main(String[] args) {
-        int[] a = {1, 2, 3};
+        int[] a = {1, 2, 3, 8};
         int[] b = {4, 5, 6, 7};
-        System.out.println("中位数是:" + findKth(a, b));
+        System.out.println("中位数是:" + findMiddle(a, b));
     }
 }
