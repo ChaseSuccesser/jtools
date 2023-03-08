433ac23471be4fe9cbd34627167522f34e7da164
ligongxing <ligongxing@meituan.com>
2023-03-08 23:51:57 +0800

    add

--- jtools-demo/src/test/java/com/ligx/list/以O1时间复杂度删除节点.java
+++ jtools-demo/src/test/java/com/ligx/list/以O1时间复杂度删除节点.java
@@ -6,22 +6,37 @@ package com.ligx.list;
  */
 public class 以O1时间复杂度删除节点 {
 
-    public void deleteNode(Node head, Node toBeDeleted) {
-        if (head == null || toBeDeleted == null) {
+    private static void deleteNode(Link head, Link toBeDeletedNode) {
+        if (head == null || toBeDeletedNode == null) {
             return;
         }
-        if (toBeDeleted.next != null) {
-            Node temp = toBeDeleted.next;
-            toBeDeleted.data = temp.data;
-            toBeDeleted.next = temp.next;
-            temp = null;
+        if (toBeDeletedNode.next != null) {
+            Link temp = toBeDeletedNode.next;
+            toBeDeletedNode.next = temp.next;
+            toBeDeletedNode.data = temp.data;
         } else {
-            Node node = head;
-            while (node.next != toBeDeleted) {
-                node = node.next;
+            Link pre = head;
+            while (pre.next != toBeDeletedNode) {
+                pre = pre.next;
             }
-            node.next = null;
-            toBeDeleted = null;
+            pre.next = null;
+        }
+    }
+
+    public static void main(String[] args) {
+        Link link6 = new Link(null, 6);
+        Link link5 = new Link(link6, 5);
+        Link link4 = new Link(link5, 4);
+        Link link3 = new Link(link4, 3);
+        Link link2 = new Link(link3, 2);
+        Link link1 = new Link(link2, 1);
+
+        deleteNode(link1, link6);
+
+        Link curr = link1;
+        while (curr != null) {
+            System.out.print(curr.data + " ");
+            curr = curr.next;
         }
     }
 }
