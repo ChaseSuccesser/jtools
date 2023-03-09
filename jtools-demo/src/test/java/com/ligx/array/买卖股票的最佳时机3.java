6e403633d94aa2f4cbfa59343aaf01f92a1c50cb
ligongxing <ligongxing@meituan.com>
2023-03-09 23:28:12 +0800

    modify

--- jtools-demo/src/test/java/com/ligx/array/买卖股票的最佳时机3.java
+++ jtools-demo/src/test/java/com/ligx/array/买卖股票的最佳时机3.java
@@ -1,14 +1,19 @@
 package com.ligx.array;
 
+import org.junit.Assert;
+
 /**
  * @author: ligongxing.
  * @date: 2023年03月06日.
  */
 public class 买卖股票的最佳时机3 {
 
-    public int maxProfit(int[] prices) {
-        int buy1 = prices[0], sell1 = 0;
-        int buy2 = prices[0], sell2 = 0;
+    private static int maxProfit(int[] prices) {
+        int buy1 = prices[0];
+        int sell1 = 0;
+        int buy2 = prices[0];
+        int sell2 = 0;
+
         for (int i = 1; i < prices.length; i++) {
             buy1 = Math.min(buy1, prices[i]);
             sell1 = Math.max(sell1, prices[i] - buy1);
@@ -16,5 +21,10 @@ public class 买卖股票的最佳时机3 {
             sell2 = Math.max(sell2, prices[i] - buy2);
         }
         return sell2;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = new int[]{3, 3, 5, 0, 0, 3, 1, 4};
+        Assert.assertTrue(maxProfit(arr) == 6);
     }
 }
