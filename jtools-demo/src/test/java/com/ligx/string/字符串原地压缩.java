5e6122d705e249b36c52a417e505052056cd345c
ligongxing <ligongxing@meituan.com>
2023-03-09 14:28:06 +0800

    add

--- jtools-demo/src/test/java/com/ligx/string/字符串原地压缩.java
+++ jtools-demo/src/test/java/com/ligx/string/字符串原地压缩.java
@@ -1,23 +1,36 @@
 package com.ligx.string;
 
+import org.apache.commons.lang.StringUtils;
+import org.junit.Assert;
+
 /**
  * Author: ligongxing.
  * Date: 2019年06月30日.
  */
 public class 字符串原地压缩 {
 
-    public static void compression(String string) {
+    public static void main(String[] args) {
+        String str = "aabbccd";
+        System.out.println(compression(str));
+        Assert.assertTrue(StringUtils.equals(compression(str), "d"));
+    }
+
+    private static String compression(String str) {
+        char[] chars = str.toCharArray();
+
         int head = 0;
         int tail = 0;
-        int count = 0;
-        char[] chars = string.toCharArray();
-        while (tail < string.length()) {
+
+        int count;
+        while (tail < str.length()) {
             count = 0;
             chars[head] = chars[tail];
-            while (tail < string.length() && chars[head] == chars[tail]) {
+
+            while (tail < str.length() && chars[head] == chars[tail]) {
                 count++;
                 tail++;
             }
+
             if (count == 1) {
                 head++;
             }
@@ -27,12 +40,6 @@ public class 字符串原地压缩 {
         for (int i = 0; i < head; i++) {
             sb.append(chars[i]);
         }
-        System.out.println(sb);
-    }
-
-
-    public static void main(String[] args) {
-        String str = "aabbccd";
-        compression(str);
+        return sb.toString();
     }
 }
