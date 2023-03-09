23154d8e7822cae6363939caa7b469c0c23741d4
ligongxing <ligongxing@meituan.com>
2023-03-09 17:27:13 +0800

    add

--- jtools-demo/src/test/java/com/ligx/array/杨氏矩阵查找.java
+++ jtools-demo/src/test/java/com/ligx/array/杨氏矩阵查找.java
@@ -1,29 +1,39 @@
 package com.ligx.array;
 
+import org.junit.Assert;
+
 /**
  * Author: ligongxing.
  * Date: 2019/06/14.
  */
 public class 杨氏矩阵查找 {
 
-    public int find(int[][] matrix, int target) {
-        if (matrix == null || matrix.length == 0) {
-            return -1;
-        }
+    private static int find(int[][] matrix, int target) {
         int rows = matrix.length;
         int columns = matrix[0].length;
 
         int row = 0;
         int column = columns - 1;
+
         while (row < rows && column >= 0) {
-            if (target == matrix[row][column]) {
+            if (matrix[row][column] == target) {
                 return matrix[row][column];
-            } else if (target < matrix[row][column]) {
-                column = column - 1;
+            } else if (matrix[row][column] > target) {
+                column--;
             } else {
-                row = row + 1;
+                row++;
             }
         }
         return -1;
+    }
+
+    public static void main(String[] args) {
+        int[][] matrix = {
+                {1, 2, 8, 9},
+                {2, 4, 9, 12},
+                {4, 7, 10, 13},
+                {6, 8, 11, 15}};
+        Assert.assertTrue(find(matrix, 4) == 4);
+        Assert.assertTrue(find(matrix, 6) == 6);
     }
 }
