f9869fd0714ff1fcb4bce2776ea79cc85019dfc4
ligongxing <ligongxing@meituan.com>
2023-03-09 21:17:43 +0800

    modify

--- jtools-demo/src/test/java/com/ligx/array/寻找旋转数组中的最小值.java
+++ jtools-demo/src/test/java/com/ligx/array/寻找旋转数组中的最小值.java
@@ -1,22 +1,30 @@
 package com.ligx.array;
 
+import org.junit.Assert;
+
 /**
  * @author: ligongxing.
  * @date: 2023年03月06日.
  */
 public class 寻找旋转数组中的最小值 {
 
-    public int findMin(int[] nums) {
-        int low = 0;
-        int high = nums.length - 1;
-        while (low < high) {
-            int middle = (low + high) / 2;
-            if (nums[middle] < nums[high]) {
-                high = middle;
+    private static int searchMin(int[] arr) {
+        int start = 0;
+        int end = arr.length - 1;
+        while (start < end) {
+            int mid = (start + end) / 2;
+            if (arr[mid] < arr[end]) { //todo
+                end = mid;
             } else {
-                low = middle + 1;
+                start = mid + 1;
             }
         }
-        return nums[low];
+        return arr[end];
+    }
+
+    public static void main(String[] args) {
+        Assert.assertTrue(searchMin(new int[]{3,4,5,1,2}) == 1);
+        Assert.assertTrue(searchMin(new int[]{4,5,6,7,0,1,3}) == 0);
+        Assert.assertTrue(searchMin(new int[]{4,5,6,1,2,3}) == 1);
     }
 }
