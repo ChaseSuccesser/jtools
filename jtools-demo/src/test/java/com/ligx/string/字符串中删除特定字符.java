d4ca4e9a56729d0268d01053ca9ab9d25d0a7919
ligongxing <ligongxing@meituan.com>
2023-03-09 14:07:05 +0800

    add

--- jtools-demo/src/test/java/com/ligx/string/字符串中删除特定字符.java
+++ jtools-demo/src/test/java/com/ligx/string/字符串中删除特定字符.java
@@ -1,12 +1,15 @@
 package com.ligx.string;
 
+import org.apache.commons.lang.StringUtils;
+import org.junit.Assert;
+
 /**
  * Author: ligongxing.
  * Date: 2019年06月30日.
  */
 public class 字符串中删除特定字符 {
 
-    public static void removeTarget(String str, char c) {
+    public static String removeTarget(String str, char c) {
         char[] chars = str.toCharArray();
 
         int head = 0;
@@ -21,11 +24,12 @@ public class 字符串中删除特定字符 {
         for (int i = 0; i < head; i++) {
             sb.append(chars[i]);
         }
-        System.out.println(sb);
+        return sb.toString();
     }
 
     public static void main(String[] args) {
-        String str = "abcdefg";
-        removeTarget(str, 'e');
+        Assert.assertTrue(StringUtils.equals(removeTarget("abcdefg", 'e'), "abcdfg"));
+        Assert.assertTrue(StringUtils.equals(removeTarget("abcde", 'e'), "abcd"));
+        Assert.assertTrue(StringUtils.equals(removeTarget("abcde", 'a'), "bcde"));
     }
 }
