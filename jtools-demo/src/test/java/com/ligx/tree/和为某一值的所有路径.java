b27f8d64eb12baa58ba72976d70bc001745e7629
ligongxing <ligongxing@meituan.com>
2023-03-12 15:00:49 +0800

    add

--- jtools-demo/src/test/java/com/ligx/tree/和为某一值的所有路径.java
+++ jtools-demo/src/test/java/com/ligx/tree/和为某一值的所有路径.java
@@ -1,5 +1,7 @@
 package com.ligx.tree;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Stack;
 
 /**
@@ -8,21 +10,39 @@ import java.util.Stack;
  */
 public class 和为某一值的所有路径 {
 
-    private static void findBinNode(Node root, int sum, Stack<Node> stack) {
+
+    public List<List<Integer>> pathSum(TreeNode root, int targetSum) {
+        Stack<Integer> stack = new Stack<>();
+        List<List<Integer>> res = new ArrayList<>();
+
+        pathSum(root, targetSum, stack, res);
+        return res;
+    }
+
+    private void pathSum(TreeNode root, int targetSum, Stack<Integer> stack, List<List<Integer>> res) {
         if (root == null) {
             return;
         }
-        if (root.data != sum) {
-            stack.push(root);
-        } else {
 
+        stack.push(root.val);
+        targetSum -= root.val;
+
+        if (root.left == null && root.right == null && targetSum == 0) {
+            Stack<Integer> tempStack = new Stack<>();
+            while (!stack.isEmpty()) {
+                tempStack.push(stack.pop());
+            }
+            List<Integer> list = new ArrayList<>();
+            while (!tempStack.isEmpty()) {
+                list.add(tempStack.peek());
+                stack.push(tempStack.pop());
+            }
+            res.add(list);
         }
-        if (root.left != null) {
-            findBinNode(root.left, sum - root.data, stack);
-        }
-        if (root.right != null) {
-            findBinNode(root.right, sum - root.data, stack);
-        }
+
+        pathSum(root.left, targetSum, stack, res);
+        pathSum(root.right, targetSum, stack, res);
+
         stack.pop();
     }
 }
