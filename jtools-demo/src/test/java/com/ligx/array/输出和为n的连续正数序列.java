6743a51e43bc4353597452e6cbaf69fbe61b0876
ligongxing <ligongxing@meituan.com>
2023-03-16 14:25:34 +0800

    modify

--- jtools-demo/src/test/java/com/ligx/array/输出和为n的连续正数序列.java
+++ jtools-demo/src/test/java/com/ligx/array/输出和为n的连续正数序列.java
@@ -1,5 +1,8 @@
 package com.ligx.array;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author: ligongxing.
  * @date: 2021年04月26日.
@@ -35,6 +38,38 @@ public class 输出和为n的连续正数序列 {
             System.out.print(i + " ");
         }
         System.out.println();
+    }
+
+    private static int[][] findContinuousSequence2(int n) {
+        int small = 1;
+        int big = 2;
+        int sum = small + big;
+        int middle = (1 + n) / 2;
+        List<int[]> vec = new ArrayList<>();
+        while (small < middle) {
+            if (sum == n) {
+                vec.add(collect(small, big));
+            }
+            while (sum > n) {
+                sum -= small;
+                small++;
+                if (sum == n) {
+                    vec.add(collect(small, big));
+                }
+            }
+            big++;
+            sum += big;
+        }
+        return vec.toArray(new int[][]{});
+    }
+
+    private static int[] collect(int small, int big) {
+        int[] arr = new int[big - small + 1];
+        int j = 0;
+        for (int i = small; i <= big; i++) {
+            arr[j++] = i;
+        }
+        return arr;
     }
 
     public static void main(String[] args) {
