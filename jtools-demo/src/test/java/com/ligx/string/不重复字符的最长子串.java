a196b488d551126732c7cc0ae3cb97f542f86af7
ligongxing <604353886@qq.com>
2024-04-20 18:38:15 +0800

    modify

--- jtools-demo/src/test/java/com/ligx/string/不重复字符的最长子串.java
+++ jtools-demo/src/test/java/com/ligx/string/不重复字符的最长子串.java
@@ -42,8 +42,36 @@ public class 不重复字符的最长子串 {
         return "";
     }
 
+    public static String longestNodupSubstring2(String str) {
+        Map<Character, Integer> cursor = new HashMap<>();
+        cursor.put(str.charAt(0), 1);
+
+        int[] lengthAt = new int[str.length()];
+        lengthAt[0] = 1;
+
+        int max = Integer.MIN_VALUE;
+        int x = 0;
+
+        for (int i = 1; i < str.length(); i++) {
+            if (cursor.containsKey(str.charAt(i))) {
+                lengthAt[i] = Math.min(lengthAt[i - 1] + 1, i - cursor.get(str.charAt(i)));
+            } else {
+                lengthAt[i] = lengthAt[i - 1] + 1;
+            }
+            if (lengthAt[i] > max) {
+                max = lengthAt[i];
+                x = i;
+            }
+            cursor.put(str.charAt(i), i);
+        }
+        if (max > 0) {
+            return str.substring(x + 1 - max, x + 1);
+        }
+        return "";
+    }
+
     public static void main(String[] args) {
         String str = "abcdeeaaccabcdef";
-        System.out.println(longestNodupSubstring(str));
+        System.out.println(longestNodupSubstring2(str));
     }
 }
